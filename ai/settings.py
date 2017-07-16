6493267c4deb825f104e6497a5330db9bbbcc553
78769488@qq.com <mybaby1112>
2017-07-16 21:54:31 +0800

    0716: 完善日志格式2

--- ai/settings.py
+++ ai/settings.py
@@ -243,8 +243,8 @@ LOGGING = {
         'info': {  # 记录到日志文件(需要创建对应的目录，否则会出错), when='h', interval=1, backupCount=0
             'level': 'INFO',
             'class': 'logging.handlers.TimedRotatingFileHandler',
-            'filename': os.path.join(BASE_DIR, "log", 'custom_info.log'),  # 日志输出文件
-            # 'filename': os.path.join(CUSTOM_INFO_LOG, 'custom_info.csv'),  # 日志输出文件
+            # 'filename': os.path.join(BASE_DIR, "log", 'custom_info.log'),  # 日志输出文件
+            'filename': os.path.join(CUSTOM_INFO_LOG, 'custom_info.csv'),  # 日志输出文件
             'when': 'midnight',
             'interval': 1,
             # 'encoding': 'utf-8',
